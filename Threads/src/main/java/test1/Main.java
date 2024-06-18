7c07927e98f40ab973b186f4cb2e041d8546ec0b
Alina Sokolova <also22092022@gmail.com>
2024-06-18 12:39:52 +0300

    Thread learning

--- Threads/src/main/java/test1/Main.java
+++ Threads/src/main/java/test1/Main.java
@@ -1,7 +1,37 @@
 package test1;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       Thread thread1=new Thread(()->{
+           for (int i=0;i<100;i++){
+               System.out.println("i="+i);
+           }
+       });
+        thread1.run();
+
+        Thread thread2=new Thread(()->{
+            for (int p=0;p<100;p++){
+                System.out.println("p="+p);
+            }
+        });
+        thread2.start();
+
+
+        for (int k=0;k<100;k++){
+            System.out.println("k="+k);
+        }
+
+    }
+
+    public class MyThread extends Thread{
+
+
+        @Override
+        public void run() {
+            super.run();
+        }
     }
 }
+
